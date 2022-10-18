974d68bd17b20b9324c735224e640ea0bb66c2ae
Rolex46 <rolexomondi46@gmail.com>
2022-10-18 07:12:02 -0700

    main.h

--- main.h
+++ main.h
@@ -3,7 +3,15 @@
 
 #include <stdlib.h>
 #include <stdarg.h>
+#include <unistd.h>
 
 int _printf(const char *format, ...);
+int _putchar(char c);
+int print_string(char *);
+int print_decimal(int value);
+long long int power(int base, int x);
+long long convertToDecimal(int Number, int base);
+int print_binary(int number);
+int print_unsigned(unsigned int number);
 
 #endif
