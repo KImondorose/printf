f6f20fee1e890948235b24bd11e1b85e73b9d04e
Rolex46 <rolexomondi46@gmail.com>
2022-10-18 07:28:15 -0700

    main.h

--- main.h
+++ main.h
@@ -5,13 +5,48 @@
 #include <stdarg.h>
 #include <unistd.h>
 
-int _printf(const char *format, ...);
-int _putchar(char c);
-int print_string(char *);
-int print_decimal(int value);
-long long int power(int base, int x);
-long long convertToDecimal(int Number, int base);
-int print_binary(int number);
-int print_unsigned(unsigned int number);
+#define UNUSED(x) (void)(x)
+#define BUFF_SIZE 1024
+
+/* FLAGS */
+#define F_MINUS 1
+#define F_PLUS 2
+#define F_ZERO 4
+#define F_HASH 8
+#define F_SPACE 16
+
+/*sizes */
+#define S_LONG 2
+#define S_SHORT 1
+
+/**
+ * struct fmt - struct op
+ *
+ * @fmt: the format.
+ * @fn: function associated.
+ */
+struct fmt
+{
+	char fmt;
+	int (*fn)(va_list, char[], int, int, int, int);
+};
+
+/**
+ * typedef struct fmt fmt_t - struct op
+ *
+ * @fmt: the format.
+ * @fm_t: the function associated
+ */
+typedef struct fmt fmt_t;
+
+int _printf(const char *format. ...);
+int handle_print(const char *fmt, int *i,
+va_list list, char buffer[], int flags, int width, int precision, int size);
+
+/****************** FUNCTIONS ******************/
+
+/* functions to print chars and strings */
+int print_char (va_list types. char buffer[],
+		int flags, int width, int precision, int size);
 
 #endif
